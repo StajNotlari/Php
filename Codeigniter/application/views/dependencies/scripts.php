c83c87fd14395c9b8c4691074bdedacf1edeb67e
Mustafa PEYNİRCİ <mustafapeynircigithub@gmail.com>
2021-08-11 17:16:29 +0300

    Codeigniter

---
+++ Codeigniter/application/views/dependencies/scripts.php
@@ -0,0 +1,5 @@
+<script src="<?php echo base_url("assets/js/jquery-3.6.0.min.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/switchery.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/custom2.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/sweetalert2.all.min.js"); ?>"></script>
